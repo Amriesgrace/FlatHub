94a987b8c6b395ef4265be04facdb76884c5dc45
Amriesgrace <obieri.grace@gmail.com>
2019-05-17 13:26:59 +0100

    Add updates to app

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     $("#view_apartments").on('change', function(ev){
-
         if($(this).val() === 'list'){
             $("#list_content").removeClass("dis-none");
             $("#location_content").addClass("dis-none");
